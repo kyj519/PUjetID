ee0c479e144de4c42002407b881788ce9b44a5e6
Nurfikri Norjoharuddeen <nurfikri89@gmail.com>
2021-09-12 15:04:45 +0200

    Use available DiLeptonSkim_ULNanoV9_v1p1 ntuples

--- Analyzer/RunLocal_SkimNtuples.sh
+++ Analyzer/RunLocal_SkimNtuples.sh
@@ -7,13 +7,18 @@ DataUL17C_DoubleMuon
 DataUL17D_DoubleMuon
 DataUL17E_DoubleMuon
 DataUL17F_DoubleMuon
-DataUL17B_DoubleEG
-DataUL17C_DoubleEG
-DataUL17D_DoubleEG
-DataUL17E_DoubleEG
-DataUL17F_DoubleEG
 MCUL17_DY_MG
-MCUL17_DY_AMCNLO
+DataUL18A_DoubleMuon
+DataUL18B_DoubleMuon
+DataUL18C_DoubleMuon
+DataUL18D_DoubleMuon
+MCUL18_DY_MG
+DataUL16APVB_DoubleMuon
+DataUL16APVC_DoubleMuon
+DataUL16APVD_DoubleMuon
+DataUL16APVE_DoubleMuon
+DataUL16APVF_DoubleMuon
+MCUL16APV_DY_MG
 )
 
 #
