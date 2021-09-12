de1f200cf4b08b0bd5d74baced67c1f6b609e246
Nurfikri Norjoharuddeen <nurfikri89@gmail.com>
2021-09-12 15:05:09 +0200

    ULNanoAODv9 update

--- Analyzer/haddHistos.sh
+++ Analyzer/haddHistos.sh
@@ -3,4 +3,6 @@
 #
 HISTODIR="histos3D"
 hadd -f ${HISTODIR}/Histo_DataUL17.root ${HISTODIR}/Histo_DataUL17*.root
+hadd -f ${HISTODIR}/Histo_DataUL18.root ${HISTODIR}/Histo_DataUL18*.root
+hadd -f ${HISTODIR}/Histo_DataUL16APV.root ${HISTODIR}/Histo_DataUL16APV*.root
 
