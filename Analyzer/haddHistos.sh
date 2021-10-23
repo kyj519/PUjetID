a873342e61dd74b59680c6db1d8c956b399432e6
Nurfikri Norjoharuddeen <nurfikri89@gmail.com>
2021-10-23 10:15:58 +0200

    Update

--- Analyzer/haddHistos.sh
+++ Analyzer/haddHistos.sh
@@ -2,7 +2,7 @@
 # combine all data histos into one root file
 #
 HISTODIR="histos3D"
-hadd -f ${HISTODIR}/Histo_DataUL17.root ${HISTODIR}/Histo_DataUL17*.root
-hadd -f ${HISTODIR}/Histo_DataUL18.root ${HISTODIR}/Histo_DataUL18*.root
-hadd -f ${HISTODIR}/Histo_DataUL16APV.root ${HISTODIR}/Histo_DataUL16APV*.root
-
+hadd -f ${HISTODIR}/Histo_DataUL17.root ${HISTODIR}/Histo_DataUL17*_*.root
+hadd -f ${HISTODIR}/Histo_DataUL18.root ${HISTODIR}/Histo_DataUL18*_*.root
+hadd -f ${HISTODIR}/Histo_DataUL16APV.root ${HISTODIR}/Histo_DataUL16APV*_*.root
+hadd -f ${HISTODIR}/Histo_DataUL16.root ${HISTODIR}/Histo_DataUL16[FGH]_*.root
