76da1ca03a46bb3d933252ba3903b53933d5cb8a
Nurfikri Norjoharuddeen <nurfikri89@gmail.com>
2021-10-25 20:19:41 +0200

    Update list of samples to run on

--- Analyzer/RunLocal_MakeHistograms.sh
+++ Analyzer/RunLocal_MakeHistograms.sh
@@ -8,17 +8,25 @@ DataUL17D_DoubleMuon
 DataUL17E_DoubleMuon
 DataUL17F_DoubleMuon
 MCUL17_DY_MG
+MCUL17_DY_AMCNLO
 DataUL18A_DoubleMuon
 DataUL18B_DoubleMuon
 DataUL18C_DoubleMuon
 DataUL18D_DoubleMuon
 MCUL18_DY_MG
+MCUL18_DY_AMCNLO
 DataUL16APVB_DoubleMuon
 DataUL16APVC_DoubleMuon
 DataUL16APVD_DoubleMuon
 DataUL16APVE_DoubleMuon
 DataUL16APVF_DoubleMuon
 MCUL16APV_DY_MG
+MCUL16APV_DY_AMCNLO
+MCUL16APV_DYToMuMu_PHG
+DataUL16F_DoubleMuon
+DataUL16G_DoubleMuon
+DataUL16H_DoubleMuon
+MCUL16_DYToMuMu_PHG
 )
 
 #
@@ -26,5 +34,5 @@ MCUL16APV_DY_MG
 #
 for SAMPLE in ${SAMPLES[@]}
 do
-  python MakeHistogramsHisto3D.py  --sample $SAMPLE --cores $NCORES --useSkimNtuples
+  python MakeHistogramsHisto3D.py --sample $SAMPLE --cores $NCORES --useSkimNtuples
 done
