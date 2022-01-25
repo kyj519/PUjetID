2269e3da87d914bdc3cdb69e64601b3eeca45d98
Nurfikri Norjoharuddeen <nurfikri89@gmail.com>
2022-01-25 20:15:12 +0100

    Add scripts to merge training ntuples

---
+++ Training/postproc/samplesInfo.py
@@ -0,0 +1,53 @@
+EOSURL      = "root://eoscms.cern.ch/"
+prod_tag    = "DiLeptonSkim_JMENanoV9_v1p2"
+path_inDir  = "/eos/cms/store/group/phys_jetmet/nbinnorj/JetPUIdTrain_"+prod_tag+"/CRABOUTPUT/"
+path_outDir = "/eos/cms/store/group/phys_jetmet/nbinnorj/JetPUIdTrain_"+prod_tag+"/MERGED/"
+
+samplesInfoDict = dict()
+
+#
+# Define cross-sections and path for all eras
+#
+eraListForMC = [
+  "UL17",
+  "UL18",
+
+]
+for era in eraListForMC:
+  samplesInfoDict["MC"+era+"_DY_MG"] = {
+    "path": [
+      "DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/JetPUIdTrain_MC"+era+"JMENanoAODv9_"+prod_tag+"*/*/*/tree_*.root"
+    ],
+    "xsec": 5347.0,
+  }
+
+
+#
+# UL2017 Lumi: 41479.68 picobarns
+# https://twiki.cern.ch/twiki/bin/view/CMS/TWikiLUM#SummaryTable
+#
+samplesInfoDict["DataUL17_DoubleMuon"] = { 
+  "path" : [
+    "DoubleMuon/JetPUIdTrain_Run2017B-DataUL17JMENanoAODv9_"+prod_tag+"/*/*/tree_*.root",
+    "DoubleMuon/JetPUIdTrain_Run2017C-DataUL17JMENanoAODv9_"+prod_tag+"/*/*/tree_*.root",
+    "DoubleMuon/JetPUIdTrain_Run2017D-DataUL17JMENanoAODv9_"+prod_tag+"/*/*/tree_*.root",
+    "DoubleMuon/JetPUIdTrain_Run2017E-DataUL17JMENanoAODv9_"+prod_tag+"/*/*/tree_*.root",
+    "DoubleMuon/JetPUIdTrain_Run2017F-DataUL17JMENanoAODv9_"+prod_tag+"/*/*/tree_*.root",
+  ],
+  "xsec":1.0,
+}
+#
+# UL2018 Lumi: 59832.47 picobarns
+# https://twiki.cern.ch/twiki/bin/view/CMS/TWikiLUM#SummaryTable
+#
+samplesInfoDict["DataUL18_DoubleMuon"] = { 
+  "path" : [
+    "DoubleMuon/JetPUIdTrain_Run2018A-DataUL18JMENanoAODv9_"+prod_tag+"/*/*/tree_*.root",
+    "DoubleMuon/JetPUIdTrain_Run2018B-DataUL18JMENanoAODv9_"+prod_tag+"/*/*/tree_*.root",
+    "DoubleMuon/JetPUIdTrain_Run2018C-DataUL18JMENanoAODv9_"+prod_tag+"/*/*/tree_*.root",
+    "DoubleMuon/JetPUIdTrain_Run2018D-DataUL18JMENanoAODv9_"+prod_tag+"/*/*/tree_*.root",
+  ],
+  "xsec":1.0,
+}
+
+
