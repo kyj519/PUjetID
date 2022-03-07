1e9b54d8898bd642a7ac7d3123b42cf3ef5092ef
yeonjoon kim <kyj519@snu.ac.kr>
2022-03-07 16:08:28 +0900

    fitting batch code

---
+++ Fitter/RunCondor_Fit.py
@@ -0,0 +1,38 @@
+import os, htcondor
+from tables import EArray
+Eras = ['UL2018','UL2017','UL2016','UL2016APV']
+orders = ['LO','NLO']
+workingPoints = ['Loose','Medium',"Tight"]
+systs = ['central', 'jesTotalUp', 'jesTotalDown']
+runkeys = [era+" "+order+" "+wp+" "+syst for syst in systs for wp in workingPoints for order in orders for era in Eras]
+condor_dir = "/u/user/yeonjoon/working_dir/PileUpJetIDSF/CMSSW_10_6_30/src/PUjetID/Fitter/condor/"
+njobs = 0
+os.system('rm -rf '+condor_dir+'job/*')
+os.system('rm -rf '+condor_dir+'log/*')
+
+print(runkeys)
+
+for runkey in runkeys:
+	f = open(condor_dir+"job/job_"+runkey.replace(" ","_")+".sh","w+")
+	f.write("#!/bin/bash\n")
+	f.write("source "+condor_dir+"../RunCondor_Fit.sh "+runkey)
+	os.system("chmod 755 "+condor_dir+"job/job_"+runkey.replace(" ","_")+".sh")
+	f.close()
+	submit_dic = {"executable": condor_dir+"job/job_"+runkey.replace(" ","_")+".sh",
+	"jobbatchname": "PUJets_"+runkey.replace(" ","_"),
+	"universe":"vanilla",
+	"request_cpus":1,
+	"log":condor_dir+"condorlog/"+runkey.replace(" ","_")+".log",
+	"getenv":"True",
+	"should_transfer_files":"YES",
+	"RequstMemory":65536,
+	"when_to_transfer_output" : "ON_EXIT",
+	"output": condor_dir+"log/"+runkey.replace(" ","_")+".log",
+	"error" : condor_dir+"log/"+runkey.replace(" ","_")+".err"}
+	sub = htcondor.Submit(submit_dic)
+	schedd = htcondor.Schedd()         
+	submit_result = schedd.submit(sub)  
+	print(submit_result)  
+	
+
+
---
+++ Fitter/RunCondor_Fit.sh
@@ -0,0 +1,26 @@
+#
+# Directory path containing all the histogram rootfiles
+#
+
+export X509_USER_PROXY=/u/user/yeonjoon/proxy.cert
+export LD_PRELOAD=/usr/lib64/libpdcap.so
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib64/dcap
+
+
+
+INDIR="gsidcap://cluster142.knu.ac.kr//pnfs/knu.ac.kr/data/cms/store/user/yeonjoon/ntuples/result_his"
+
+#
+# Directory path for the output of the fits
+#
+#OUTDIR="gsidcap://cluster142.knu.ac.kr//pnfs/knu.ac.kr/data/cms/store/user/yeonjoon/ntuples/result_his/fitresult"
+OUTDIR="/u/user/yeonjoon/working_dir/PileUpJetIDSF/CMSSW_10_6_30/src/PUjetID/Fitter/result"
+#runkeyformat = era+" "+order+" "+wp+" "+syst
+#############################################################################
+#
+#
+#
+#############################################################################
+
+python /u/user/yeonjoon/working_dir/PileUpJetIDSF/CMSSW_10_6_30/src/PUjetID/Fitter/extract_fit.py --input $INDIR --output ${OUTDIR}/${2}_${4}/${1}_WP${3}/  --year $1 --wp $3 --syst $4 --order $2
+ 
