cb4d72f019a780ce648969a8761834ab72bcbceb
yeonjoon kim <kyj519@snu.ac.kr>
2022-03-19 18:46:34 +0900

    miscellanous change

---
+++ Skimmer/crab_UL/SendToCrab_AMCNLO_JetBinned.sh
@@ -0,0 +1,4 @@
+python crab_UL2017_MC.py ../samples/ULNanoAODv9/ULNanoAODv9_2017_MC_amcnlo_jetBinned.txt
+python crab_UL2016_MC.py ../samples/ULNanoAODv9/ULNanoAODv9_2016_MC_amcnlo_jetBinned.txt
+python crab_UL2016APV_MC.py ../samples/ULNanoAODv9/ULNanoAODv9_2016APV_MC_amcnlo_jetBinned.txt
+python crab_UL2018_MC.py ../samples/ULNanoAODv9/ULNanoAODv9_2018_MC_amcnlo_jetBinned.txt
